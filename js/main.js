38a9d8adba92d983f8dc33c495b357fa7aeb6d86
dreadfulstep <dreadfulstep@gmail.com>
2026-08-20 13:34:15 +0100

    Forgot what i last commited yesterday so just making sure its all here

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ import { initInput } from "./input.js";
 
 const overlay = document.getElementById('start-overlay');
 
-overlay.addEventListener('click', async () => {
+document.addEventListener('DOMContentLoaded', async () => {
+        initInput();
+
+});
+
+overlay?.addEventListener('click', async () => {
     await Tone.start();
     await engine.init();
-    initInput();
-    overlay.style.display = 'none';
     console.log('Music Studio ready. A-K to play.');
 })
